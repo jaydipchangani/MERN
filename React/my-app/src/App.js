fa2b7e59ff203ae9a2b99ca07519f0df7cb2377d
jaydip <pjaydip956@gmail.com>
2025-07-24 15:41:30 +0530

    set theme

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,30 +1,14 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-import axios from 'axios';
 
 function App() {
 
-  const [user,setUser] = useState([])
+  const[isDark, setIsDark]= useState(false);
   
-  /*
-  useEffect(()=>{
-    fetch('https://jsonplaceholder.typicode.com/todos')
-    .then(res=>res.json() )
-    .then(data=> {setUser(data); console.log(data);} )
-    .catch(err=> console.log(err))
-  },[])
-  */
-
-  useEffect(()=>{
-    axios.get('https://jsonplaceholder.typicode.com/todos')
-    .then(res=>setUser(res.data))
-    .catch(err=> console.log(err))
-  },[])
-
   return (
-    <div >
-      <h3>User Data</h3>
-    {user.map(u => <li key={u.id}>{u.title}</li>)}
+    <div style={{ background: isDark ? "#333" : "#fff", color: isDark ? "#fff" : "#000", padding: "20px" }}>
+       <h2>{isDark ? "Dark Mode" : "Light Mode"}</h2>
+       <button onClick={()=>setIsDark(!isDark)}>Toggle Theme</button>
     </div>
   );
 }
