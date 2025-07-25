946da20155e3d4da369034b340b0634adc594469
jaydip <pjaydip956@gmail.com>
2025-07-25 17:34:33 +0530

    Add stopwatch functionality to ComponentB and enhance ComponentC with color change feature

--- React/my-app/src/components/ComponentB.jsx
+++ React/my-app/src/components/ComponentB.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import ComponentC from './ComponentC'
 
 const ComponentB = () => {
+    const [time,setTime]= useState(0)
+    let timeRef = useRef(null)
+
+    function startStopWatch(){
+        timeRef.current=setInterval(()=>{
+            setTime(time=>time+1)
+        },1000)
+    }
+
+    function stopStopWatch(){
+        clearInterval(timeRef.current)
+        timeRef.current=null
+    }
+
+    function resetStopWatch(){
+        stopStopWatch()
+        setTime(0)
+    }
   return (
     <div>
       <ComponentC />
+      <h3>StopWatch {time} Seconds</h3>
+      
+      <button onClick={startStopWatch}>Start</button>
+      <br/>
+      <button onClick={stopStopWatch}>Stop</button>
+      <br/>
+      <button onClick={resetStopWatch}>Reset</button>
       
     </div>
   )
--- React/my-app/src/components/ComponentC.jsx
+++ React/my-app/src/components/ComponentC.jsx
@@ -1,4 +1,4 @@
-import {React, useContext} from 'react'
+import {React, useContext, useRef} from 'react'
 import { UserContext } from './UseContextExample';
 import { ThemeContext } from './ComponentA';
 
@@ -6,12 +6,20 @@ const ComponentC = () => {
     const user = useContext(UserContext); // Consuming context
 
     const myTheme=useContext(ThemeContext)
+    const btnColor =useRef()
+    function changeColor(){
+        btnColor.current.style.backgroundColor="Red"
+        btnColor.current.style.width="200px"
+    }
 
   return (
     <div style={{background: myTheme.theme ? "#123465" : "#654321"}}>
       Hello, {user.name}!<p></p>
       {myTheme.theme}
       <button onClick={()=>myTheme.setTheme(!myTheme.theme)}> {myTheme.theme ? "Light" : "Dark"}</button>
+      
+      <button ref={btnColor}>Look Here !!</button>
+      <button onClick={changeColor}>Change Color</button>
     </div>
   )
 }
