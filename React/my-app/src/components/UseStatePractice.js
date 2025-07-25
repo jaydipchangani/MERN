11680e9dee89957d9ce6afcdbd69b27559f19543
jaydip <pjaydip956@gmail.com>
2025-07-25 10:11:13 +0530

    Refactor UseStatePractice component to implement user search functionality with GitHub API

--- React/my-app/src/components/UseStatePractice.js
+++ React/my-app/src/components/UseStatePractice.js
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function UseStatePractice()
 {
+    /*
     const[form,setForm]= useState({
         name:'',
         email:''
@@ -22,9 +23,10 @@ function UseStatePractice()
         e.preventDefault();
         console.log(form)
     }
-    return(
+
+     return(
         <>
-            <h3>Form</h3>
+    <h3>Form</h3>
 
             <form onSubmit={handlSubmit}>
                 <input type="text" name="name"placeholder="Enter Name" value={form.name} onChange={handleChange}/>
@@ -35,6 +37,42 @@ function UseStatePractice()
 
             <h3>{form.name}</h3>
             <h3>{form.email}</h3>
+
+            </>
+*/
+
+    const[query, setQuery] = useState("")
+    const[users,setUsers] =useState([])
+
+    useEffect(()=>{
+         if (!query.trim()) {
+    setUsers([]);
+    return; 
+  }
+
+        fetch(`https://api.github.com/search/users?q=${query}`)
+        .then(res=>res.json())
+        .then(data => {setUsers(data.items);})
+        .catch(err => console.log(err))
+    },[query])
+
+    return(
+        <>
+            <form>
+                <input type="text" value={query} onChange={(e)=>setQuery(e.target.value)}/>
+            </form>
+
+            <ul>
+        {users.map((user) => (
+          <li key={user.id} style={{ marginTop: "10px" }}>
+            <img src={user.avatar_url} alt="avatar" width="30" />
+            &nbsp;
+            <a href={user.html_url} target="_blank" rel="noreferrer">
+              {user.login}
+            </a>
+          </li>
+        ))}
+      </ul>
         </>
     )
 }
