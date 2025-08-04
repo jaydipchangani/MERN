bacb942dcb39e64770bb2d6ef6de1d4eb5b8db90
jaydip <pjaydip956@gmail.com>
2025-08-04 12:00:06 +0530

    Refactor API component for improved error handling and loading state management

--- React/my-app/src/Practice/useEffect/API.jsx
+++ React/my-app/src/Practice/useEffect/API.jsx
@@ -1,23 +1,43 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 function API() {
-    const [user,setUser] = useState([])
+  const [user, setUser] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
-    useEffect(()=>{
-        fetch('https://fake.jsonmockapi.com/products?length=10')
-        .then(res => res.json())
-        .then(data => setUser(data))
-        .catch(err => console.log(err))
-},[])
+  useEffect(() => {
+    fetch("https://fake.jsonmockapi.com/products?length=50")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Not ok");
+        }
+
+        return res.json();
+      })
+      .then((data) => {
+        setUser(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError("Error occurs");
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <ul>
-        {
-            user.map(u => (<li key={u.sku}>{u.productName} - ${u.price}</li>))
-        }
-      </ul>
+      {loading ? (
+        <h1>Loading......</h1>
+      ) : error ? (
+        <h1>Error Happens</h1>
+      ) : (
+        user.map((u) => (
+          <li key={u.sku}>
+            {u.productName} - ${u.price}
+          </li>
+        ))
+      )}
     </div>
-  )
+  );
 }
 
-export default API
+export default API;
